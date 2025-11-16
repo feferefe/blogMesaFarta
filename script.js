ebe0e450ff0197a51b193f0102db058fb4a4303c
Fernanda Rodrigues da Silva <feferodrigueees@gmail.com>
2025-11-16 14:03:50 -0300

    feat: Versão inicial do blog Mesa Farta

---
+++ script.js
@@ -0,0 +1,33 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    fetch("/components/_header.html") 
+        .then(response => response.text()) 
+        .then(data => {
+            const headerPlaceholder = document.getElementById("header-placeholder")
+            if (headerPlaceholder) headerPlaceholder.innerHTML = data;
+        })
+
+    fetch("/components/_footer.html") 
+        .then(response => response.text()) 
+        .then(data => {
+            const footerPlaceholder = document.getElementById("footer-placeholder")
+            if (footerPlaceholder) footerPlaceholder.innerHTML = data;
+        })
+    
+    const contactForm = document.getElementById("contact-form")
+    const formWrapper = document.getElementById("form-wrapper")
+    const obrigadoMsg = document.getElementById("obrigado-mensagem")
+
+    if (contactForm && formWrapper && obrigadoMsg) {
+        
+        contactForm.addEventListener("submit", function(event) {
+            
+            event.preventDefault(); 
+
+            formWrapper.style.display = "none"
+            obrigadoMsg.style.display = "block"
+        
+            window.scrollTo(0, 0)
+        })
+    }
+})
